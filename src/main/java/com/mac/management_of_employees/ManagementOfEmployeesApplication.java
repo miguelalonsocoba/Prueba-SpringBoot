a4eaf31bd8791091deb2cf98aeeb3d4336b4ee89
mac <miguetheking53@gmail.com>
2025-02-11 18:55:10 -0600

    Inicializando proyecto

---
+++ src/main/java/com/mac/management_of_employees/ManagementOfEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.mac.management_of_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementOfEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementOfEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mac/management_of_employees/ManagementOfEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mac.management_of_employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementOfEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
